99d152d51c77fc3226d4b686048a168ee3837e28
unknown <Poonam.Nagarajan@expleogroup.com>
2023-08-23 21:10:56 +0530

    Test Autothon - Final

---
+++ gitCommands.bat
@@ -0,0 +1,10 @@
+:: =========================================
+::  Bat file for the triggering the git commands
+:: =========================================
+
+git status
+git add .
+git commit -m "Test Autothon - Final"
+git push origin main
+
+
