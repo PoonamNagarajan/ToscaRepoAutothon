13afe88008fa484ecd97b79f840c9f64bf9ac8a1
unknown <Poonam.Nagarajan@expleogroup.com>
2023-08-24 02:16:59 +0530

    Test Autothon - Final

--- gitCommands.bat
+++ gitCommands.bat
@@ -2,7 +2,7 @@
 ::  Bat file for the triggering the git commands
 :: =========================================
 
-cd C:\Data\GitRepo\ToscaRepoAutothon
+
 git status
 git add .
 git commit -m "Test Autothon - Final"
